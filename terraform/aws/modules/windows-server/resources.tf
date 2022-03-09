2adf1dac3c272a239b84f0df690550de281da9b4
wallylambic <williamshannondavis@gmail.com>
2022-03-10 08:21:26 +1100

    Updates

--- terraform/aws/modules/windows-server/resources.tf
+++ terraform/aws/modules/windows-server/resources.tf
@@ -14,15 +14,15 @@ data "aws_ami" "latest-windows-server-2019" {
   }
 }
 
-
 resource "aws_instance" "windows_server" {
-  count         = var.config.windows_server == "1" ? 1 : 0
+  count         = "${var.config.windows_server}"
   ami           = data.aws_ami.latest-windows-server-2019[count.index].id
   instance_type          = var.config.aws_tier == "free" ? "t2.micro" : var.config.instance_type_ec2
   key_name = var.config.key_name
   subnet_id = var.ec2_subnet_id
-  private_ip             = var.config.windows_server_private_ip
   vpc_security_group_ids = [var.vpc_security_group_ids]
+  private_ip             = "10.0.1.2${count.index}"
+  depends_on             = [var.windows_domain_controller_instance]
   tags = {
     Name = "ar-win-server-${var.config.range_name}-${var.config.key_name}--${count.index}"
   }
@@ -45,16 +45,17 @@ EOF
       type     = "winrm"
       user     = "Administrator"
       password = var.config.attack_range_password
-      host     = aws_instance.windows_server[count.index].public_ip
+      host     = "${self.public_ip}"
       port     = 5986
       insecure = true
-      https    = true
+      https    = true 
       timeout  = "10m"
-        }
+    }
   }
 
   provisioner "local-exec" {
     working_dir = "../../../ansible"
-    command = "ansible-playbook -i '${aws_instance.windows_server[count.index].public_ip},' playbooks/windows_dc.yml --extra-vars 'splunk_indexer_ip=${var.config.splunk_server_private_ip} ansible_user=Administrator ansible_password=${var.config.attack_range_password} win_password=${var.config.attack_range_password} splunk_uf_win_url=${var.config.splunk_uf_win_url} win_sysmon_url=${var.config.win_sysmon_url} win_sysmon_template=${var.config.win_sysmon_template} splunk_admin_password=${var.config.attack_range_password} splunk_stream_app=${var.config.splunk_stream_app} s3_bucket_url=${var.config.s3_bucket_url} win_4688_cmd_line=${var.config.win_4688_cmd_line} verbose_win_security_logging=${var.config.verbose_win_security_logging} install_red_team_tools=${var.config.install_red_team_tools}'"
+    command = "ansible-playbook -i '${self.public_ip},' playbooks/windows_server.yml --extra-vars 'aws_tier=${var.config.aws_tier} splunk_headend_ip=${var.config.splunk_headend_ip} splunk_indexer_ip=${var.config.splunk_server_private_ip} ansible_user=Administrator ansible_password=${var.config.attack_range_password} win_password=${var.config.attack_range_password} splunk_uf_win_url=${var.config.splunk_uf_win_url} win_sysmon_url=${var.config.win_sysmon_url} win_sysmon_template=${var.config.win_sysmon_template} splunk_admin_password=${var.config.attack_range_password} windows_domain_controller_private_ip=${var.config.windows_domain_controller_private_ip} windows_server_join_domain=${var.config.windows_server_join_domain} splunk_stream_app=${var.config.splunk_stream_app} s3_bucket_url=${var.config.s3_bucket_url} win_4688_cmd_line=${var.config.win_4688_cmd_line} verbose_win_security_logging=${var.config.verbose_win_security_logging} install_red_team_tools=${var.config.install_red_team_tools}'"
   }
+
 }
