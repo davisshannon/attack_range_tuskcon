fd8fbbae85e677a283eae7bf50d62c90a116697f
davisshannon <williamshannondavis@gmail.com>
2022-03-10 11:32:19 +1100

    Update variable.tf

--- terraform/aws/modules/zeek_sensor/variable.tf
+++ terraform/aws/modules/zeek_sensor/variable.tf
@@ -2,6 +2,5 @@
 variable "config" { }
 variable "vpc_security_group_ids" { }
 variable "ec2_subnet_id" { }
-variable "windows_server_instance" { }
 variable "windows_domain_controller_instance" { }
 variable "windows_client_instance" { }
